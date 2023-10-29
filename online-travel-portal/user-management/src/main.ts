05a7537e371f23c0ac9e7d0500f1c07371daed2d
Komal Gupta <komgupta1@WKMZTA0ADD4F.global.publicisgroupe.net>
2023-10-29 13:47:52 +0530

    implement rate limiting

--- online-travel-portal/user-management/src/main.ts
+++ online-travel-portal/user-management/src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'user';
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 10, // Limit each IP to 100 requests per window
+    }),
+  );
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
